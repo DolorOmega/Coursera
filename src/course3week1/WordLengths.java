2665dbc12fda5e4a063bd07f33e7533f14d0b71a
Jeremy <jdfoster02@gmail.com>
2016-06-05 23:38:08 -0500

    3.1 updates

---
+++ src/course3week1/WordLengths.java
@@ -0,0 +1,66 @@
+package course3week1;
+
+import edu.duke.FileResource;
+
+public class WordLengths {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WordLengths wl = new WordLengths();
+		wl.testCountWordLengths();
+	}
+	
+	public int[] countWordLengths(FileResource resource, int[] counts){
+		for (String word : resource.words()){
+			char firstChar = word.charAt(0);
+			char lastChar = word.charAt(word.length()-1);
+			System.out.println(lastChar);
+			
+			if (!Character.isAlphabetic(firstChar)){
+				System.out.println("first char non alpha word: " + word);
+				word.substring(1, word.length());
+			}
+			if (!Character.isAlphabetic(lastChar)){
+				System.out.println("last char non alpha word: " + word);
+				word.substring(0, word.length()-1);
+				System.out.println("word after last char substring: " + word);
+			}
+			
+			if (word.length() > 0){
+				counts[word.length()-1]++;
+				System.out.println("Word length incremented" + counts[word.length()]);
+			}else{
+				counts[0]++;
+			}
+		}
+		
+		return counts;
+	}
+	
+	public void testCountWordLengths(){
+		FileResource fr = new FileResource();
+		int maxCountIndex = 0;
+		
+		int[] counts = countWordLengths(fr, new int[31]);
+		maxCountIndex = indexOfMax(counts);
+		
+		for (int i = 0; i < counts.length; i++){
+			System.out.println("Words of length " + (i + 1) + ": " + counts[i]);
+		}
+		
+		System.out.println(maxCountIndex);
+	}
+	
+	public int indexOfMax(int[] values){
+		int maxIndex = 0;
+		
+		for (int i = 0; i < values.length; i++){
+			if (values[i] < values[maxIndex]){
+				maxIndex = i;
+			}
+		}
+		
+		return maxIndex;
+	}
+
+}
--- src/course3week2/WordFrequencies.java
+++ src/course3week2/WordFrequencies.java
@@ -3,8 +3,13 @@ package course3week2;
 import java.util.ArrayList;
 
 public class WordFrequencies {
-	private ArrayList myWords = new ArrayList<String>();
-	private ArrayList myFreqs = new ArrayList<Integer>();
+	private ArrayList myWords;
+	private ArrayList myFreqs;
+	
+	public WordFrequencies(ArrayList myWords, ArrayList myFreqs){
+		myWords = new ArrayList<String>();
+		myFreqs = new ArrayList<Integer>();
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
